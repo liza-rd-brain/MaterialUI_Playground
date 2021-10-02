fd9c0ddb1887a5bf6a9246d4daa592c2dd015f07
Elizabeth <33760912+MinakovaLisa@users.noreply.github.com>
2021-10-02 21:22:25 +0300

    test passing props

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { styled, createTheme, ThemeProvider } from "@mui/system";
+import { makeStyles } from "@mui/styles";
+
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { Slider } from "@mui/material";
+import Delete from "@mui/icons-material/Delete";
+
+const useAppStyles = makeStyles({
+  container: {
+    margin: "50px",
+
+    " & > * ": {
+      marginBottom: "30px",
+    },
+  },
+
+  button: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+  },
+});
+
+const useIconStyles = makeStyles({
+  icon: {
+    color: (props: IconType) => (props.done ? "green" : "red"),
+  },
+});
+
+type IconType = {
+  done: Boolean;
+};
+
+const testObj = {
+  itemList: [{ done: false }, { done: true }],
+};
+
+function DeleteIcon(props: IconType) {
+  const classes = useIconStyles(props);
+  return <Delete className={classes.icon} />;
+}
+
+function App() {
+  const classes = useAppStyles();
+
+  return (
+    <Container className={classes.container}>
+      <Button className={classes.button}>Hello World</Button>
+      <Slider />
+      <>
+        {testObj.itemList.map((item, index) => {
+          return <DeleteIcon done={item.done} key={index} />;
+        })}
+      </>
+    </Container>
+  );
+}
+
+export default App;
